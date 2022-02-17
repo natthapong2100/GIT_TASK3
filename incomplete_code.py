25b11f7bade23eb6a81fd85a6b4243f8528f2042
Natthapong Lueangphumphitthaya <thenat2015@gmail.com>
2022-02-17 18:08:55 +0700

    mod file

--- incomplete_code.py
+++ incomplete_code.py
@@ -1,38 +1,38 @@
 class Transportation(object):
-   """Abstract base class"""
+    """Abstract base class"""
 
-   def __init__( self, start, end, distance ):
-      if self.__class__ == Transportation:
-         raise NotImplementedError
-      self.start = start
-      self.end = end
-      self.distance = distance
+    def init(self, start, end, distance):
+        if self.class == Transportation:
+            raise NotImplementedError
+        self.start = start
+        self.end = end
+        self.distance = distance
 
-   def find_cost( self ):
-      """Abstract method; derived classes must override"""
-      raise NotImplementedError
-
-
-class Walk( Transportation ):
-
-   def __init__( self, start, end, distance ):
-      Transportation.__init__( self, start, end, distance)
-
-   def find_cost( self ):
-      return 0
-      pass
+    def find_cost(self):
+        """Abstract method; derived classes must override"""
+        raise NotImplementedError
 
 
-   
+class Walk(Transportation):
+
+    def init(self, start, end, distance):
+        Transportation.init(self, start, end, distance)
+
+    def find_cost(self):
+        return 0
+
+
+
+
 # main program
-bird = "hello bird"
+
 travel_cost = 0
 
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         #Train("Ladkrabang Station","Payathai Station",40,6),
-         #Taxi("Payathai Station","The British Council",3) ]
+trip = [Walk("KMITL", "KMITL SCB Bank", 0.6),
+        Taxi("KMITL SCB Bank", "Ladkrabang Station", 5),
+        Train("Ladkrabang Station", "Payathai Station", 40, 6),
+        Taxi("Payathai Station", "The British Council", 3)]
 
 for travel in trip:
-   travel_cost += travel.find_cost()
-print travel_cost
+    travel_cost += travel.find_cost()
+print(travel_cost)
